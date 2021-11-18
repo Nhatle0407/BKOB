50a149682a410b82fa61721e18135f07eddf4967
Le Nguyen Khang <khang.lenguyen192@gmail.com>
2021-11-18 16:37:56 +0700

    update account and change info

--- app/src/main/java/com/example/bkob/views/fragments/AccountFragment.java
+++ app/src/main/java/com/example/bkob/views/fragments/AccountFragment.java
@@ -2,9 +2,11 @@ package com.example.bkob.views.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,15 +20,46 @@ import com.example.bkob.databinding.FragmentCartBinding;
 import com.example.bkob.views.activity.AuthenticationActivity;
 import com.example.bkob.views.activity.MainActivity;
 import com.example.bkob.views.activity.SplashActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountFragment extends Fragment {
     FragmentAccountBinding binding;
+    TextView userName, userEmail, userPhoneNumber;
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    DatabaseReference databaseReference;
+    DatabaseReference userRef;
+
+    boolean isLogin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentAccountBinding.inflate(getLayoutInflater());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            firebaseUser = firebaseAuth.getCurrentUser();
+            databaseReference = FirebaseDatabase.getInstance("https://bkob-a0229-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("users");
+            userRef = databaseReference.child(firebaseUser.getUid());
+            isLogin = true;
+        }
+        else {
+            isLogin = false;
+        }
+
         return binding.getRoot();
     }
 
@@ -35,6 +68,9 @@ public class AccountFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.VISIBLE);
 
+        if (isLogin){
+            displayUserInfo();
+        }
         setUpButton();
 
     }
@@ -50,22 +86,37 @@ public class AccountFragment extends Fragment {
         binding.btnYourProducts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new SellingFragment());
-                getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                if (isLogin){
+                    replaceFragment(new SellingFragment());
+                    getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                }
+                else {
+                    Toast.makeText(getContext(), "Hãy đăng nhập trước", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         binding.btnOrderReceive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                replaceFragment(new ReceiveFragment());
-                getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                if (isLogin){
+                    replaceFragment(new ReceiveFragment());
+                    getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                }
+                else {
+                    Toast.makeText(getContext(), "Hãy đăng nhập trước", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         binding.btnShoppingHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new BuyHistoryFragment());
-                getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                if (isLogin){
+                    replaceFragment(new BuyHistoryFragment());
+                    getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                }
+                else {
+                    Toast.makeText(getContext(), "Hãy đăng nhập trước", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         binding.btnSupport.setOnClickListener(new View.OnClickListener() {
@@ -78,15 +129,25 @@ public class AccountFragment extends Fragment {
         binding.btnChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new ChangePasswordFragment());
-                getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                if (isLogin){
+                    replaceFragment(new ChangePasswordFragment());
+                    getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                }
+                else {
+                    Toast.makeText(getContext(), "Hãy đăng nhập trước", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         binding.btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new ChangeInfoFragment());
-                getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                if (isLogin){
+                    replaceFragment(new ChangeInfoFragment());
+                    getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                }
+                else {
+                    Toast.makeText(getContext(), "Hãy đăng nhập trước", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         binding.btnLogOut.setOnClickListener(new View.OnClickListener() {
@@ -100,6 +161,44 @@ public class AccountFragment extends Fragment {
     private void logOut(){
         Intent intent = new Intent(getActivity(), AuthenticationActivity.class);
         startActivity(intent);
+        if (isLogin){
+            firebaseAuth.signOut();
+        }
         getActivity().finish();
+    }
+
+    private void displayUserInfo(){
+        userName = binding.userName;
+        userEmail = binding.userEmail;
+        userPhoneNumber = binding.userPhoneNumber;
+
+        userEmail.setText(firebaseUser.getEmail());
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String uid = ds.getKey();
+                    String value = ds.getValue().toString();
+
+                    switch (uid){
+                        case "name":
+                            userName.setText(value);
+                            break;
+                        case "phone":
+                            userPhoneNumber.setText(value);
+                            break;
+                        default:
+                            break;
+                    }
+                    Log.d("account", uid + ":  " + value);
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("account", databaseError.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bkob/views/fragments/ChangeInfoFragment.java
+++ app/src/main/java/com/example/bkob/views/fragments/ChangeInfoFragment.java
@@ -1,9 +1,13 @@
 package com.example.bkob.views.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,17 +17,47 @@ import androidx.fragment.app.FragmentManager;
 import com.example.bkob.R;
 import com.example.bkob.databinding.FragmentChangeInfoBinding;
 import com.example.bkob.databinding.FragmentReceiveBinding;
+import com.example.bkob.models.SignUpModel;
 import com.example.bkob.views.adapters.ReceiveAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class ChangeInfoFragment extends Fragment {
 
     FragmentChangeInfoBinding binding;
+    EditText editName, editPhoneNumber, editAddress;
+    TextView userEmail;
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    DatabaseReference databaseReference;
+    DatabaseReference userRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentChangeInfoBinding.inflate(getLayoutInflater());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance("https://bkob-a0229-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("users");
+        userRef = databaseReference.child(firebaseUser.getUid());
+
+
+
+        displayUserInfo();
+
         return binding.getRoot();
     }
 
@@ -37,6 +71,13 @@ public class ChangeInfoFragment extends Fragment {
             }
         });
 
+        binding.btnChangeInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeUserData(firebaseUser);
+            }
+        });
+
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -44,5 +85,66 @@ public class ChangeInfoFragment extends Fragment {
         fragmentManager.beginTransaction()
                 .replace(R.id.mainFragments, fragment)
                 .commit();
+    }
+
+    private void displayUserInfo(){
+        editName = binding.editName;
+        editAddress = binding.editAddress;
+        editPhoneNumber = binding.editPhoneNumber;
+        userEmail = binding.userEmail;
+
+        userEmail.setText(firebaseUser.getEmail());
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String uid = ds.getKey();
+                    String value = ds.getValue().toString();
+
+                    switch (uid){
+                        case "address":
+                            editAddress.setText(value);
+                            break;
+                        case "name":
+                            editName.setText(value);
+                            break;
+                        case "phone":
+                            editPhoneNumber.setText(value);
+                            break;
+                        default:
+                            break;
+                    }
+                    Log.d("account", uid + ":  " + value);
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("account", databaseError.getMessage());
+            }
+        });
+    }
+
+    private void changeUserData(FirebaseUser user) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("userId", ""+ user.getUid());
+        hashMap.put("name", ""+ editName.getText());
+        hashMap.put("email", ""+ userEmail.getText());
+        hashMap.put("phone", ""+ editPhoneNumber.getText());
+        hashMap.put("address", ""+ editAddress.getText());
+        hashMap.put("avatar", "");
+        DatabaseReference userRef = FirebaseDatabase.getInstance("https://bkob-a0229-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("users");
+        userRef.child(user.getUid()).setValue(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(getContext(), "Thay đổi thông tin thành công", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Thay đổi thất bại. Xin hãy thử lại", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
